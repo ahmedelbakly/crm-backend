f63c46351230cf91526a7f2bce0d2fdfabb311f6
ahmed elbakly <78669298+ahmedelbakly@users.noreply.github.com>
2024-03-18 05:30:28 +0200

    cors

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 const prisma = new PrismaClient();
+const cors = require("cors");
+const { ObjectId } = require("mongodb");
+// handle cors
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    preflightContinue: true,
+  })
+);
 
 const port = 5000;
 //express json middleware
@@ -13,31 +24,142 @@ app.get("/api/greeting", (req, res) => {
   res.json({ greeting: "Hello API" });
 });
 app.get("/api/tasks", async (req, res) => {
+  const tasks = await prisma.task.findMany();
 
-  const tasks = await prisma.task.findMany()
-  
-    res.status(200).send(tasks)
-  
+  res.status(200).send(tasks);
 });
 
 app.post("/api/task", async (req, res, next) => {
   try {
-    const { name, assignTo,adminId } = req.body
-    const newTask = await prisma.task
-      .create({
+    const {
+      name,
+      assignTo,
+      adminId,
+      startDate,
+      endDate,
+      status,
+      description,
+      priority,
+      relatedTo,
+    } = req.body;
+
+    // return res.status(201).json({
+    //   message: "task created successfully",
+    //   data: (req.body.startDate).toISOString,
+    // })
+    const newTask = await prisma.task.create({
+      data: {
+        name: name,
+        assignTo: assignTo,
+        adminId: adminId,
+        startDate: new Date(startDate),
+        endDate: new Date(endDate),
+        status: status,
+        description: description,
+        priority: priority,
+        relatedTo: relatedTo,
+      },
+    });
+    const tasks = await prisma.task.findMany();
+    res.status(201).send({
+      message: "successfully",
+      payload: tasks,
+    });
+  } catch (e) {
+    res.status(202).send({
+      message: "failed",
+      error: "your data  is not valid",
+    });
+  }
+});
+// handle update request to /api/task/:id with a function that updates a task in the database using Prisma
+app.put("/api/task/:id", async (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    assignTo,
+    adminId,
+    startDate,
+    endDate,
+    status,
+    description,
+    priority,
+    relatedTo,
+  } = req.body;
+  try {
+    if (
+      !name ||
+      !assignTo ||
+      !adminId ||
+      !startDate ||
+      !endDate ||
+      !status ||
+      !description ||
+      !priority ||
+      !relatedTo
+    ) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    } else {
+      await prisma.task.update({
+        where: {
+          id: id,
+        },
         data: {
           name: name,
-          assignTo:assignTo,
-          adminId:adminId
+          assignTo: assignTo,
+          adminId: adminId,
+          startDate: new Date(startDate),
+          endDate: new Date(endDate),
+          status: status,
+          description: description,
+          priority: priority,
+          relatedTo: relatedTo,
         },
-      })
-      res.status(201).send(newTask)
-      
+      });
+      // ACCESS ALL AFTER UPDATING TASKS
+      const tasks = await prisma.task.findMany();
+      // send updated task back as response
+      res.status(201).json({
+        message: "successfully",
+        payload: tasks,
+      });
+    }
   } catch (e) {
-    console.log(e);
+    res.status(400).json({
+      message: "failed",
+      error: e.message,
+    });
   }
 });
 
+// handle delete request to /api/task/:id with a function that deletes a task from the database using Prisma
+app.delete("/api/task/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.task.delete({
+      where: {
+        id: id,
+      },
+    });
+    const tasks = await prisma.task.findMany();
+
+    res.status(201).json({
+      message: "successfully",
+      payload: tasks,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "failed",
+      error: error.message,
+    });
+  }
+
+  // send updated task back as response
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}/api/greeting`);
